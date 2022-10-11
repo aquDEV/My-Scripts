f3ce67e17ddcf5b07df1c9cddbf26232f41cd3f6
aquDEV <98266807+aquDEV@users.noreply.github.com>
2022-10-12 02:19:44 +0300

    Add files via upload

---
+++ SceneLoader.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    //Loads a scene
+  public void LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+    //Quits the game
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is shutting down");
+    }
+}
+
